5f1a5bc820663c995e061a946e424b1d89415e72
shenyachuan <shenya0203@163.com>
2026-07-14 16:44:15 +0800

    添加1200 2400 4800 波特率设置

--- views/Uart.vue
+++ views/Uart.vue
@@ -37,6 +37,9 @@
         <div class="form-group">
           <label>{{ t('uart.baudRate') }}:</label>
           <select v-model.number="uartList[activeTab].baud_rate">
+            <option :value="1200">1200</option>
+            <option :value="2400">2400</option>
+            <option :value="4800">4800</option>
             <option :value="9600">9600</option>
             <option :value="19200">19200</option>
             <option :value="38400">38400</option>
